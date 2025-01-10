31027f66947950183ffa5d3eaa461e105bc26deb
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-10 22:46:36 +0100

    addTwo

--- 0-javascript_fundamentals/0x00-javascript-basics/7-add_two.js
+++ 0-javascript_fundamentals/0x00-javascript-basics/7-add_two.js
@@ -1,1 +1,17 @@
+// TEST CASE
+/*
+const addTwo = require('../7-add_two');
+const {assert} = require('chai');
 
+it('should add two', () => {
+    assert.equal(addTwo(2), 4);
+    assert.equal(addTwo(6), 8);
+    assert.equal(addTwo(10), 12);
+});
+*/
+
+function addTwo(input) {
+    return input + 2;
+}
+
+module.exports = addTwo;
