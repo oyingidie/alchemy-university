753a83cf892710d87bbe47e401a8658440f0dd21
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-10 13:26:52 +0100

    A is a number

---
+++ 0-javascript_fundamentals/0x00-javascript-basics/0-number_variable.js
@@ -0,0 +1,15 @@
+const a = 12.5
+
+module.exports = a;
+
+
+// Test Case
+
+/*
+const assert = require('assert');
+const a = require('../index');
+
+it('the variable a should store a number', () => {
+    assert.equal(typeof a, "number");
+});
+*/
