dac9b5e095147bb8f513f4ff0e28305e37de4014
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-10 15:45:17 +0100

    Is it over? False!

--- 0-javascript_fundamentals/0x00-javascript-basics/2-booleans.js
+++ 0-javascript_fundamentals/0x00-javascript-basics/2-booleans.js
@@ -1,12 +1,7 @@
-const badPeople = false;
-const greatNation = true;
-
-module.exports = { a, b }
-
-// TEST Case
+// TEST CASE
 /*
 const assert = require('assert');
-const {a,b} = require('../index');
+const {a, b} = require('../2-booleans');
 
 it('a should be boolean', () => {
     assert.equal(typeof a, 'boolean');
@@ -17,6 +12,11 @@ it('b should be boolean', () => {
 });
 
 it('a should not equal b', () => {
-    assert.notEqual(a,b);
+    assert.notEqual(a, b);
 });
 */
+
+const a = false;
+const b = true;
+
+module.exports = { a, b }
