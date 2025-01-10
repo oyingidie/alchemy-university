6ee187885ed701e88723f6ecb33418b2eea96df6
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-10 18:27:49 +0100

    Fix the comments, human!

---
+++ 0-javascript_fundamentals/0x00-javascript-basics/5-comments.js
@@ -0,0 +1,14 @@
+// TEST CASE
+/*
+const assert = require('assert');
+const shoesCost = require('../5-comments');
+
+it('shoesCost should be 49', () => {
+    assert.equal(shoesCost, 49);
+});
+*/
+
+// all shoes are on sale this week!
+const shoesCost = 49;
+
+module.exports = shoesCost;
