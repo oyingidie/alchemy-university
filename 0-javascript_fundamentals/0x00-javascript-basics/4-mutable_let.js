168b744267269d17ecf7cfaf889bb14f1a68ddfe
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-10 18:06:51 +0100

    Let's be the change we desire

---
+++ 0-javascript_fundamentals/0x00-javascript-basics/4-mutable_let.js
@@ -0,0 +1,15 @@
+// TEST CASE
+/*
+const assert = require('assert');
+const a = require('../4-mutable_let');
+
+it('a should equal 5', () => {
+    assert.equal(a, 5);
+});
+*/
+
+let a = 3;
+
+a = 5;
+
+module.exports = a;
