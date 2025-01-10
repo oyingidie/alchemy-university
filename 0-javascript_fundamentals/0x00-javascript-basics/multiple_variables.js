9bda655420624812625a69186cf40c8aa0e9176b
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-10 14:15:40 +0100

    Two equal variables

---
+++ 0-javascript_fundamentals/0x00-javascript-basics/multiple_variables.js
@@ -0,0 +1,13 @@
+const
+const 
+
+module.exports = { a, b }
+
+// Test Case
+/*  const assert = require('assert');
+    const {a,b} = require('../index');
+
+    it('a and b should be equal', () => {
+        assert.equal(a,b);
+    });
+*/
