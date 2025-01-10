3e483fdfd4ae24f8d56679d99e039546e497add3
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-10 15:41:11 +0100

    Two equal variables are better than one

--- 0-javascript_fundamentals/0x00-javascript-basics/1-multiple_variables.js
+++ 0-javascript_fundamentals/0x00-javascript-basics/1-multiple_variables.js
@@ -1,13 +1,14 @@
+// TEST CASE
+/*
+const assert = require('assert');
+const {a, b} = require('../1-multiple_variables');
+
+it('a and b should be equal', () => {
+    assert.equal(a, b);
+});
+*/
+
 const a = 16;
 const b = a;
 
 module.exports = { a, b }
-
-// Test Case
-/*  const assert = require('assert');
-    const {a,b} = require('../index');
-
-    it('a and b should be equal', () => {
-        assert.equal(a,b);
-    });
-*/
