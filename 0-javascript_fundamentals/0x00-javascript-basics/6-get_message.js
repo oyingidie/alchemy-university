1d3424a49823c36699f78c7ec12ffc2336cab86c
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-10 22:09:57 +0100

    Functional!

---
+++ 0-javascript_fundamentals/0x00-javascript-basics/6-get_message.js
@@ -0,0 +1,16 @@
+// TEST CASE
+/*
+const getMessage = require('../getMessage');
+const {assert} = require('chai');
+
+it('should return a message', () => {
+    assert(getMessage(), "it looks like nothing was returned from getMessage")
+    assert.equal(typeof getMessage(), "string", "a string should be returned from getMessage");
+});
+*/
+
+function getMessage() {
+     return "This message can be recycled.";
+}
+
+module.exports = getMessage;
