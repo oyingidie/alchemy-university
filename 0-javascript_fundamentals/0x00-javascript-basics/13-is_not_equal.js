a10baca4d1ebf55ce8490847eb7732cc61f7b00d
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-11 21:21:16 +0100

    Not another check

---
+++ 0-javascript_fundamentals/0x00-javascript-basics/13-is_not_equal.js
@@ -0,0 +1,13 @@
+// TEST CASE
+/*
+const isNotEqual = require('../13-is_not_equal');
+const { assert } = require('chai');
+
+describe('isNotEqual', () => {
+    describe('if the values are not equal', () => {
+        it('should return true', () => {
+            assert.equal(isNotEqual(1, 2), true);
+        });
+    });
+});
+*/
