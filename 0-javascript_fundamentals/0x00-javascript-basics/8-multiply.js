3c41bfffca0997c27b33f3f3ae9dff0b945136a4
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-11 20:33:44 +0100

    The result is a product

---
+++ 0-javascript_fundamentals/0x00-javascript-basics/8-multiply.js
@@ -0,0 +1,17 @@
+// TEST CASE
+/*
+const product = require('../8-multiply');
+const {assert} = require('chai');
+
+it('should multiple two numbers', () => {
+    assert.equal(product(2,2), 4);
+    assert.equal(product(6,10), 60);
+    assert.equal(product(10,22), 220);
+});
+*/
+
+function product(a, b) {
+    return a * b;
+}
+
+module.exports = product;
