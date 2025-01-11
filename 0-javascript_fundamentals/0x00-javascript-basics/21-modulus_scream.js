2fb84ee6e3cc80b30a983f5762bf85284225efbb
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-11 23:24:34 +0100

    A Screaming Good Job

---
+++ 0-javascript_fundamentals/0x00-javascript-basics/21-modulus_scream.js
@@ -0,0 +1,34 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const scream = require('../21-modulus_scream');
+
+describe('scream', () => {
+    it('should do a scream of length 1', () => {
+        assert.equal(scream(1), "a");
+    });
+    it('should do a scream of length 5', () => {
+        assert.equal(scream(5), "aAaAa");
+    });
+    it('should do a scream of length 10', () => {
+        assert.equal(scream(10), "aAaAaAaAaA");
+    });
+});
+*/
+
+function scream(n) {
+    let str = "";
+
+    for (let i = 1; i <= n; i++) {
+        const isEven = i % 2 === 0;
+
+        if (isEven) {
+            str += "A";
+        } else {
+            str += "a";
+        }
+    }
+    return str;
+}
+
+module.exports = scream;
