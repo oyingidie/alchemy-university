52f3a25b21fe75945d7d2b676d53cc9846dfb90d
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-12 20:11:45 +0100

    X marks the spot!

--- 0-javascript_fundamentals/0x01-javascript-data_storage/0-starts_with_x.js
+++ 0-javascript_fundamentals/0x01-javascript-data_storage/0-starts_with_x.js
@@ -1,1 +1,27 @@
+// TEST CASE
+/*
+const startsWithX = require('../0-starts_with_x');
+const { assert } = require('chai');
 
+describe('startsWithX', () => {
+    it('should return true for a string starting with x', () => {
+        assert.equal(startsWithX("x"), true);
+        assert.equal(startsWithX("xpizza"), true);
+    });
+
+    it('should return false for a string not starting with x', () => {
+        assert.equal(startsWithX("pizza"), false);
+        assert.equal(startsWithX("zyx"), false);
+    });
+});
+*/
+
+function startsWithX(string) {
+    if (string.charAt(0) === "x") {
+        return true;
+    }
+
+    return false;
+}
+
+module.exports = startsWithX;
