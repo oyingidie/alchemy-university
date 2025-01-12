e87fd8c0ab6580c14e084b36990bfcb03caf8058
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-12 12:47:50 +0100

    Even so...

---
+++ 0-javascript_fundamentals/#-practice_problems/pset_0.js
@@ -0,0 +1,40 @@
+// TEST CASE 0
+/*
+const isEven = require('../pset_0');
+const { assert } = require('chai');
+
+describe('isEven', () => {
+    describe('even numbers', () => {
+        it('should return true', () => {
+            assert.equal(isEven(2), true);
+        });
+        it('should return true', () => {
+            assert.equal(isEven(4), true);
+        });
+        it('should return true', () => {
+            assert.equal(isEven(10), true);
+        });
+    });
+    describe('odd numbers', () => {
+        it('should return false', () => {
+            assert.equal(isEven(1), false);
+        });
+        it('should return false', () => {
+            assert.equal(isEven(3), false);
+        });
+        it('should return false', () => {
+            assert.equal(isEven(9), false);
+        });
+    });
+});
+*/
+
+// PROBLEM 0
+function isEven(num) {
+    if (num % 2 === 0) {
+        return true;
+    }
+    return false;
+}
+
+module.exports = isEven;
