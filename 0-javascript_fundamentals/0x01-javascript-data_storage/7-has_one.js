95a931b48197bdf3c9e99eaafd23d78606cf59fa
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-13 06:06:25 +0100

    A little like strings

---
+++ 0-javascript_fundamentals/0x01-javascript-data_storage/7-has_one.js
@@ -0,0 +1,34 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const hasOne = require('../hasOne');
+
+describe('hasOne', () => {
+    it('should handle arrays with 1 in it', () => {
+        assert(hasOne([1]));
+        assert(hasOne([1, 2, 3]));
+        assert(hasOne([3, 1, 2]));
+        assert(hasOne([3, 2, 1]));
+        assert(hasOne([1, 1]));
+        assert(hasOne([1, 1, 1]));
+    });
+
+    it('should handle arrays without 1 in it', () => {
+        assert(!hasOne([]));
+        assert(!hasOne([2, 3, 4]));
+        assert(!hasOne([6, 7, 8, 9, 10, 11]));
+    });
+});
+*/
+
+function hasOne(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === 1) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+module.exports = hasOne;
