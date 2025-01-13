926320ac2afe98bafacba5d638a7fa8ed637e5ba
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-13 05:56:05 +0100

    1, 2, 3 and we're off

---
+++ 0-javascript_fundamentals/0x01-javascript-data_storage/6-create_array.js
@@ -0,0 +1,19 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const array = require('../6-create_array');
+
+describe('array', () => {
+    it('should be an array of length 3', () => {
+        assert.equal(array.length, 3);
+    });
+
+    it('should contain elements 1, 2, 3', () => {
+        assert.sameMembers(array, [1, 2, 3]);
+    });
+});
+*/
+
+const array = [1, 2, 3]; // <-- create an array here!
+
+module.exports = array;
