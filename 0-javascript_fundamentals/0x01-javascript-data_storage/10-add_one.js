547c2b2811e0ccbd7113f36d8e849c9f9faa9632
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-14 21:23:35 +0100

    Spectacular

---
+++ 0-javascript_fundamentals/0x01-javascript-data_storage/10-add_one.js
@@ -0,0 +1,36 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const addOne = require('../10-add_one');
+
+describe('addOne', () => {
+    it('should handle a single element array', () => {
+        const array = [1];
+        const returned = addOne(array);
+        assert.equal(returned, undefined, "the function should not return anything");
+        assert.sameMembers(array, [2]);
+    });
+
+    it('should handle an array with a few sequential elements', () => {
+        const array = [1, 2, 3];
+        const returned = addOne(array);
+        assert.equal(returned, undefined, "the function should not return anything");
+        assert.sameMembers(array, [2, 3, 4]);
+    });
+
+    it('should handle a larger array', () => {
+        const array = [9, 12, 14, 16, 19];
+        const returned = addOne(array);
+        assert.equal(returned, undefined, "the function should not return anything");
+        assert.sameMembers(array, [10, 13, 15, 17, 20]);
+    });
+});
+*/
+
+function addOne(array) {
+    for (let i = 0; i < array.length; i++) {
+        array[i]++;
+    }
+}
+
+module.exports = addOne;
