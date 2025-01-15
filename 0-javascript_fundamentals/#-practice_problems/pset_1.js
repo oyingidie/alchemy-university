b364612fb4d2be5f65ae70519ed3d96750b7d47b
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-15 18:26:21 +0100

    Brought to you by the letter C

--- 0-javascript_fundamentals/#-practice_problems/pset_1.js
+++ 0-javascript_fundamentals/#-practice_problems/pset_1.js
@@ -23,3 +23,19 @@ function halfValue(numbers) {
 }
 
 module.exports = halfValue;
+
+// CODE 2
+
+function countC(str) {
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i].toLowerCase() === "c") {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+module.exports = countC;
