f7a66bc7025caf05df22a9b31c451327e519eee5
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-15 16:11:40 +0100

    Hidden secret...

---
+++ 0-javascript_fundamentals/0x01-javascript-data_storage/18-secret_key.js
@@ -0,0 +1,19 @@
+// TEST CASE
+/*
+const removeSecret = require('../18-secret_key');
+const { assert } = require('chai');
+
+describe('removeSecret', () => {
+    it('should remove the secret', () => {
+        const person = {
+            name: "Alice",
+            secret: "afraid of the dark"
+        }
+
+        removeSecret(person);
+
+        assert.equal(person.name, "Alice");
+        assert.equal(person.secret, undefined);
+    });
+});
+*/
