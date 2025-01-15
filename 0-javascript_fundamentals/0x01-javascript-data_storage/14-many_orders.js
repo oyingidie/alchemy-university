1d0f0786e5b50aae48ca65e58e435c1d8629fc08
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-15 13:42:16 +0100

    Got all the pizzas

---
+++ 0-javascript_fundamentals/0x01-javascript-data_storage/14-many_orders.js
@@ -0,0 +1,29 @@
+// TEST CASE
+/*
+const numberOfPizzas = require('../14-many_orders');
+const { assert } = require('chai');
+
+describe('numberOfPizzas', () => {
+    describe('a single order', () => {
+        const orders = [
+            { pizzas: 3 },
+        ]
+
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(orders), 3);
+        });
+    });
+
+    describe('many orders', () => {
+        const orders = [
+            { pizzas: 3 },
+            { pizzas: 5 },
+            { pizzas: 10 },
+        ]
+
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(orders), 18);
+        });
+    });
+});
+*/
