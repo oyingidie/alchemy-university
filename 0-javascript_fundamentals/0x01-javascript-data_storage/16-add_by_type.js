29a7445610f31e371d02a7055f06220d51c03bd8
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-15 14:36:09 +0100

    No more pizza

---
+++ 0-javascript_fundamentals/0x01-javascript-data_storage/16-add_by_type.js
@@ -0,0 +1,49 @@
+// TEST CASE
+/*
+const numberOfPizzas = require('../16-add_by_type');
+const { assert } = require('chai');
+const ORDER_TYPES = require('../orderTypes');
+
+describe('numberOfPizzas', () => {
+    describe('just pizza', () => {
+        const orders = [
+            { pizzas: 3, type: ORDER_TYPES.PIZZA, extraCheese: true },
+            { pizzas: 5, type: ORDER_TYPES.PIZZA, extraCheese: false },
+            { pizzas: 10, type: ORDER_TYPES.PIZZA, extraCheese: true },
+        ]
+
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(orders), 18);
+        });
+    });
+
+    const keys = Object.keys(ORDER_TYPES);
+    describe(keys.join(', '), () => {
+        const orders = keys.reduce((arr, key) => {
+            return arr.concat({
+                type: key,
+            });
+        }, [{ pizzas: 3, type: ORDER_TYPES.PIZZA }]);
+
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(orders), 3);
+        });
+    });
+});
+*/
+
+const ORDER_TYPES = require('../orderTypes');
+
+function numberOfPizzas(orders) {
+    let total = 0;
+
+    for (let i = 0; i < orders.length; i++) {
+        if (orders[i].type === ORDER_TYPES.PIZZA) {
+            total += orders[i].pizzas;
+        }
+    }
+
+    return total;
+}
+
+module.exports = numberOfPizzas;
