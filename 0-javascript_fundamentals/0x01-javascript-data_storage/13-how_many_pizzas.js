45bab449a18762e8b18d249c04e9b1c83761f2cb
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-15 13:11:57 +0100

    Pizza retrieval complete...

---
+++ 0-javascript_fundamentals/0x01-javascript-data_storage/13-how_many_pizzas.js
@@ -0,0 +1,36 @@
+// TEST CASE
+/*
+const numberOfPizzas = require('../13-how_many_pizzas');
+const { assert } = require('chai');
+
+describe('order', () => {
+    describe('1 pizzas', () => {
+        const order = {
+            pizzas: 1,
+            extraCheese: true,
+            deliveryInstructions: "Round the back, please!",
+        }
+
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(order), order.pizzas);
+        });
+    });
+
+    describe('5 pizzas', () => {
+        const order = {
+            pizzas: 5,
+            extraCheese: false,
+            deliveryInstructions: "Call when you arrive",
+        }
+
+        it('should return the number of pizzas', () => {
+            assert.equal(numberOfPizzas(order), order.pizzas);
+        });
+    });
+});
+*/
+function numberOfPizzas(order) {
+    return order.pizzas;
+}
+
+module.exports = numberOfPizzas;
