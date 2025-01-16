a875133b01f037ee70d2dc52f22a184163444b51
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-16 22:30:08 +0100

    How?

--- 0-javascript_fundamentals/#-practice_problems/test_1.js
+++ 0-javascript_fundamentals/#-practice_problems/test_1.js
@@ -172,3 +172,22 @@ describe('sumTogether', () => {
         assert.deepEqual(result, [25, 45, 65]);
     })
 })
+
+// TEST CASE 7
+
+const {assert} = require('chai');
+const countElements = require('../pset_1');
+
+describe('countElements', () => {
+    it('should return an empty object', () => {
+        const elements = [];
+        const result = countElements(elements);
+        assert.deepEqual(result, {});
+    })
+
+    it('should return an object of non-duplicative element counts', () => {
+        const elements = ["a", "a", "b", "c", "b", "d"];
+        const result = countElements(elements);
+        assert.deepEqual(result, {a: 2, b: 2, c: 1, d: 1})
+    })
+})
