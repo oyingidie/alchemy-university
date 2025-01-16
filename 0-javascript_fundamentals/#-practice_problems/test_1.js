bc4bc4a442691ad2595b8a6c21989241d0bab259
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-16 19:48:53 +0100

    Palindrome

--- 0-javascript_fundamentals/#-practice_problems/test_1.js
+++ 0-javascript_fundamentals/#-practice_problems/test_1.js
@@ -116,3 +116,38 @@ describe('reverse', () => {
         assert.equal(reverse('macintosh'), 'hsotnicam');
     });
 })
+
+// TEST CASE 5
+
+const { assert } = require('chai');
+const isPalindrome = require('../pset_1');
+
+describe('isPalindrome', () => {
+    describe('is a palindrome', () => {
+        it('should return true', () => {
+            assert.equal(isPalindrome('pop'), true);
+        });
+
+        it('should return true', () => {
+            assert.equal(isPalindrome('kayak'), true);
+        });
+
+        it('should return true', () => {
+            assert.equal(isPalindrome('racecar'), true);
+        });
+    })
+    
+    describe('is not a palindrome', () => {
+        it('should return false', () => {
+            assert.equal(isPalindrome('bear'), false);
+        });
+
+        it('should return false', () => {
+            assert.equal(isPalindrome('pizza'), false);
+        });
+
+        it('should return false', () => {
+            assert.equal(isPalindrome('representative'), false);
+        });
+    })
+})
