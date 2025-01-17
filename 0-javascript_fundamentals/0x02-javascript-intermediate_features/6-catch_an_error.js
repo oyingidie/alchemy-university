ee09f89c4b5bf3a1811c3c52cd50633a6b80a7c5
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-17 19:09:05 +0100

    Caught in the act!

---
+++ 0-javascript_fundamentals/0x02-javascript-intermediate_features/6-catch_an_error.js
@@ -0,0 +1,30 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const catchError = require('../6-catch_an_error');
+
+describe('catchError', () => {
+    it('should catch a thrown error', () => {
+        let ex;
+        try {
+            catchError(() => {
+                throw new Error();
+            });
+        }
+        catch (_ex) {
+            ex = _ex;
+        }
+        assert(!ex, "the error should have been caught");
+    });
+});
+*/
+
+function catchError(fn) {
+    try {
+        fn();
+    }
+
+    catch(exception) {}
+}
+
+module.exports = catchError;
