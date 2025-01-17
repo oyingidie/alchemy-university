de55f16d1ba0c1565bde1032e8fd59a2b9b4791c
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-17 17:52:29 +0100

    Car crossing...

---
+++ 0-javascript_fundamentals/0x02-javascript-intermediate_features/4-not_operator.js
@@ -0,0 +1,29 @@
+// TEST CASE
+/*
+const carCrossing = require('../4-not_operator');
+const { assert } = require('chai');
+
+describe('carCrossing', () => {
+    describe('when a is crossing and not b', () => {
+        it('should allow a to cross', () => {
+            assert.equal(carCrossing(true, false), true);
+        });
+    });
+    describe('when both are crossing', () => {
+        it('should be stalemate', () => {
+            assert.equal(carCrossing(true, true), false);
+        });
+    });
+    describe('when b is crossing and not a', () => {
+        it('should allow b to cross', () => {
+            assert.equal(carCrossing(false, true), true);
+        });
+    });
+    describe('when neither are crossing', () => {
+        it('should not be true', () => {
+            assert.equal(carCrossing(false, false), false);
+        });
+    });
+});
+*/
+
