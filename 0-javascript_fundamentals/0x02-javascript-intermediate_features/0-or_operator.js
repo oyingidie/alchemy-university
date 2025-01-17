909f791de7bf71bbddc823e52873129048e65f44
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-17 16:51:39 +0100

    Junk Food Time

--- 0-javascript_fundamentals/0x02-javascript-intermediate_features/0-or_operator.js
+++ 0-javascript_fundamentals/0x02-javascript-intermediate_features/0-or_operator.js
@@ -1,1 +1,35 @@
+// TEST CASE
+/*
+const willEat = require('../0-or_operator');
+const { assert } = require('chai');
 
+describe('willEat', () => {
+    describe('all variables are true', () => {
+        it('should be true', () => {
+            assert.equal(willEat(true, true, true), true);
+        });
+    });
+    describe('one variable is true', () => {
+        it('should be true', () => {
+            assert.equal(willEat(true, false, false), true);
+            assert.equal(willEat(false, true, false), true);
+            assert.equal(willEat(false, false, true), true);
+        });
+    });
+    describe('no variables are true', () => {
+        it('should be false', () => {
+            assert.equal(willEat(false, false, false), false);
+        });
+    });
+});
+*/
+
+function willEat(hasPizza, hasDonuts, hasCookies) {
+    if (hasPizza || hasDonuts || hasCookies) {
+        return true;
+    }
+
+    return false;
+}
+
+module.exports = willEat;
