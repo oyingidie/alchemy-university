18c6e0061a9da6eb1717f16aa2c4099d6d80fcda
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-18 18:20:32 +0100

    Binding

---
+++ 0-javascript_fundamentals/0x03-javascript-classes_and_prototypes/1-binding_this.js
@@ -0,0 +1,16 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const thisName = require('../1-binding_this');
+
+describe('thisName', () => {
+    it('should be bound to an object with the name Bob', () => {
+        assert.equal(thisName(), 'Bob');
+    });
+
+    it('should have renamed the function bound thisName', () => {
+        assert.equal(thisName.name, "bound thisName");
+    });
+});
+*/
+
