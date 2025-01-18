bf031db2fad3a27ef645806d418b1a0755b11a58
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-18 01:33:47 +0100

    Coerced

---
+++ 0-javascript_fundamentals/0x02-javascript-intermediate_features/10-to_string.js
@@ -0,0 +1,34 @@
+// TEST CASE
+/*
+const combineToString = require('../10-to_string');
+const { assert } = require('chai');
+
+describe('combineToString', () => {
+    describe('given two numbers', () => {
+        it('should combine them', () => {
+            assert.strictEqual(combineToString(1,1), "11");
+            assert.strictEqual(combineToString(5,10), "510");
+        });
+    });
+    describe('given a number and a string', () => {
+        it('should combine them', () => {
+            assert.strictEqual(combineToString(2, "2"), "22");
+            assert.strictEqual(combineToString("5", 55), "555");
+        });
+    });
+    describe('given a number and a boolean', () => {
+        it('should combine them', () => {
+            assert.strictEqual(combineToString(2, true), "2true");
+            assert.strictEqual(combineToString(false, 55), "false55");
+        });
+    });
+});
+*/
+
+function combineToString(a, b) {
+    const string = b.toString();
+
+    return a + string;
+}
+
+module.exports = combineToString;
