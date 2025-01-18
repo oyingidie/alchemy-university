224b5db562a0829a075cc875992702cd7a48e2b9
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-18 17:25:55 +0100

    Make a wishful sound

--- 0-javascript_fundamentals/#-practice_problems/test_2.js
+++ 0-javascript_fundamentals/#-practice_problems/test_2.js
@@ -47,5 +47,39 @@ describe('eitherNotBoth', () => {
     });
 })
 
+// TEST CASE 1
 
+const {assert} = require('chai');
+const fizzBuzz = require('../pset_2');
 
+describe('fizzBuzz', () => {
+    it('should return an empty string', () => {
+        const numbers = [1, 2, 4, 7, 11];
+        const result = fizzBuzz(numbers);
+        assert.equal(result, "");
+    })
+
+    it('should return fizz', () => {
+        const numbers = [3, 6, 9, 12];
+        const result = fizzBuzz(numbers);
+        assert.equal(result, "fizzfizzfizzfizz");
+    })
+
+    it('should return buzz', () => {
+        const numbers = [5, 10, 20, 25];
+        const result = fizzBuzz(numbers);
+        assert.equal(result, "buzzbuzzbuzzbuzz");
+    })
+
+    it('should return fizzbuzz', () => {
+        const numbers = [15, 30, 45];
+        const result = fizzBuzz(numbers);
+        assert.equal(result, "fizzbuzzfizzbuzzfizzbuzz");
+    })
+
+    it('should return all conditions', () => {
+        const numbers = [1, 3, 5, 9, 11, 15, 17, 20];
+        const result = fizzBuzz(numbers);
+        assert.equal(result, "fizzbuzzfizzfizzbuzzbuzz");
+    })
+})
