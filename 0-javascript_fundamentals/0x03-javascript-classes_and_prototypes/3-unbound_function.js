d0d36dd761853b52e453a4839228cb67a8b9c0e8
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-19 22:13:31 +0100

    Unbound

---
+++ 0-javascript_fundamentals/0x03-javascript-classes_and_prototypes/3-unbound_function.js
@@ -0,0 +1,31 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const Celebrity = require('../3-unbound_function');
+
+describe('Celebrity', () => {
+    describe('Will Smith', () => {
+        const celebrity = new Celebrity("Will Smith");
+
+        it('should set their name', () => {
+            assert.equal(celebrity.name, "Will Smith");
+        });
+
+        it('should set their age', () => {
+            assert.equal(celebrity.age, 51);
+        });
+    });
+
+    describe('Matt Damon', () => {
+        const celebrity = new Celebrity("Matt Damon");
+
+        it('should set their name', () => {
+            assert.equal(celebrity.name, "Matt Damon");
+        });
+
+        it('should set their age', () => {
+            assert.equal(celebrity.age, 49);
+        });
+    });
+});
+*/
