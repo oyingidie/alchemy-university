1eab18baf1310740eaa83db649a71eef120eaf7d
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-20 16:46:32 +0100

    Super warrior

---
+++ 0-javascript_fundamentals/0x03-javascript-classes_and_prototypes/13-super_warrior.js
@@ -0,0 +1,35 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const Warrior = require('../13-super_warrior');
+const Hero = require('../11-take_damage');
+
+describe('new Warrior', () => {
+    const warrior = new Warrior();
+
+    it('should have 50 health', () => {
+        assert.equal(warrior.health, 50);
+    });
+
+    it('should be a hero', () => {
+        assert(warrior instanceof Hero);
+    });
+
+    it('should have 0 rage', () => {
+        assert.equal(warrior.rage, 0);
+    });
+});
+
+describe('new Hero', () => {
+    const hero = new Hero();
+
+    it('should have 50 health', () => {
+        assert.equal(hero.health, 50);
+    });
+
+    it('should not have rage', () => {
+        assert.equal(hero.rage, undefined);
+    });
+});
+*/
+
