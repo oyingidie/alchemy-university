747d6d51de821d5e9df60b7b5c1549bd7a0c0c88
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-20 16:33:21 +0100

    New hero!

---
+++ 0-javascript_fundamentals/0x03-javascript-classes_and_prototypes/11-take_damage.js
@@ -0,0 +1,40 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const Hero = require('../11-take_damage');
+
+describe('new Hero', () => {
+    const hero = new Hero();
+
+    describe('after taking 5 damage', () => {
+        before(() => {
+            hero.takeDamage(5);
+        });
+
+        it('should have 45 health', () => {
+            assert.equal(hero.health, 45);
+        });
+
+        describe('after taking 20 more damage', () => {
+            before(() => {
+                hero.takeDamage(20);
+            });
+
+            it('should have 25 health', () => {
+                assert.equal(hero.health, 25);
+            });
+        });
+    });
+});
+*/
+
+class Hero {
+    constructor() {
+        this.health = 50;
+    }
+    takeDamage(number) {
+        this.health -= number;
+    }
+}
+
+module.exports = Hero;
