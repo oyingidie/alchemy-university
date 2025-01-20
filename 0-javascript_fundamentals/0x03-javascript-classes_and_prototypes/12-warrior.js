d3f125249e141c2520c14fd11168d4f4e2b0b317
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-20 16:36:38 +0100

    Extend hero...

---
+++ 0-javascript_fundamentals/0x03-javascript-classes_and_prototypes/12-warrior.js
@@ -0,0 +1,26 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const Warrior = require('../12-warrior');
+const Hero = require('../11-take_damage');
+
+describe('new Warrior', () => {
+    const warrior = new Warrior();
+    
+    it('should have 50 health', () => {
+        assert.equal(warrior.health, 50);
+    });
+
+    it('should be a hero', () => {
+        assert(warrior instanceof Hero);
+    });
+});
+*/
+
+const Hero = require('./Hero');
+
+class Warrior extends Hero {
+
+}
+
+module.exports = Warrior;
