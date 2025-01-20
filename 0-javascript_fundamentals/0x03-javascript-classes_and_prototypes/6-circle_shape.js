bd7d8c95cdee296ae06b598839dc89b952ff233f
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-20 11:59:16 +0100

    Running circles...

---
+++ 0-javascript_fundamentals/0x03-javascript-classes_and_prototypes/6-circle_shape.js
@@ -0,0 +1,23 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const Circle = require('../6-circle_shape');
+
+describe('Circle', () => {
+    let circle;
+    let x = 10;
+    let y = 15;
+    let radius = 15;
+    describe('instance', () => {
+        beforeEach(() => {
+            circle = new Circle(x, y, radius);
+        });
+
+        it('should set the properties', () => {
+            assert.equal(circle.position.x, x);
+            assert.equal(circle.position.y, y);
+            assert.equal(circle.radius, radius);
+        });
+    });
+}); 
+*/
