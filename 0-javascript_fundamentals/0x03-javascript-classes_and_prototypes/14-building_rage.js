68071c45b5d29f4f3c9357ee0e1062942fa3e150
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-20 16:55:43 +0100

    Out-rage-ous!

---
+++ 0-javascript_fundamentals/0x03-javascript-classes_and_prototypes/14-building_rage.js
@@ -0,0 +1,77 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const Warrior = require('../14-building_rage');
+const Hero = require('../11-take_damage');
+
+describe('new Warrior', () => {
+    const warrior = new Warrior();
+
+    it('should have 50 health', () => {
+        assert.equal(warrior.health, 50);
+    });
+
+    it('should be a hero', () => {
+        assert(warrior instanceof Hero);
+    });
+
+    it('should have 0 rage', () => {
+        assert.equal(warrior.rage, 0);
+    });
+
+    describe('after taking 5 damage', () => {
+        before(() => {
+            warrior.takeDamage(5);
+        });
+
+        it('should have 45 health', () => {
+            assert.equal(warrior.health, 45);
+        });
+
+        it('should have 1 rage', () => {
+            assert.equal(warrior.rage, 1);
+        });
+
+        describe('after taking 15 more damage', () => {
+            before(() => {
+                warrior.takeDamage(15);
+            });
+
+            it('should have 30 health', () => {
+                assert.equal(warrior.health, 30);
+            });
+
+            it('should have 2 rage', () => {
+                assert.equal(warrior.rage, 2);
+            });
+        });
+    });
+});
+
+describe('new Hero', () => {
+    const hero = new Hero();
+
+    it('should have 50 health', () => {
+        assert.equal(hero.health, 50);
+    });
+
+    it('should not have rage', () => {
+        assert.equal(hero.rage, undefined);
+    });
+});
+*/
+
+const Hero = require('./Hero');
+
+class Warrior extends Hero {
+    constructor() {
+        super();
+        this.rage = 0;
+    }
+    takeDamage(number) {
+        super.takeDamage(number);
+        this.rage++;
+    }
+}
+
+module.exports = Warrior;
