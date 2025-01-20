dd4466b07ea119d38fcc9074978a6eadcbd37193
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-20 10:59:55 +0100

    Moving helps us stay in shape!

--- 0-javascript_fundamentals/0x03-javascript-classes_and_prototypes/5-move_shape.js
+++ 0-javascript_fundamentals/0x03-javascript-classes_and_prototypes/5-move_shape.js
@@ -1,9 +1,31 @@
 // TEST CASE
 /*
 const { assert } = require('chai');
-const Shape = require('../Shape');
+const Shape = require('../5-move_shape');
 
 let shape;
 let x = 5;
 let y = 10;
+let moveX = 20;
+let moveY = 30;
+describe('Shape Instance', () => {
+    beforeEach(() => {
+        shape = new Shape(x, y);
+    });
+
+    it('should set the initial position coordinates', () => {
+        assert.equal(shape.position.x, x);
+        assert.equal(shape.position.y, y);
+    });
+
+    it('should have a function called move', () => {
+        assert.equal(typeof shape.move, "function");
+    });
+
+    it('should handle a move', () => {
+        shape.move(moveX, moveY);
+        assert.equal(shape.position.x, x + moveX);
+        assert.equal(shape.position.y, y + moveY);
+    });
+});
 */
