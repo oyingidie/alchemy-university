0eea4972d845a569d98261e9c42d22dce20a6a10
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-22 20:08:27 +0100

    Array sort

---
+++ 0-javascript_fundamentals/0x04-javascript-array_manipulation/5-sort_by_month.js
@@ -0,0 +1,58 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const sortByMonth = require('../5-sort_by_month');
+
+describe('sortByMonth', () => {
+    it('should fix the sort order for three events', () => {
+        let actual = [
+            { event: 'dance', month: 'MAR' },
+            { event: 'farmers market', month: 'JUN' },
+            { event: 'parade', month: 'JAN' },
+        ];
+        sortByMonth(actual);
+        const expected = [
+            { event: 'parade', month: 'JAN' },
+            { event: 'dance', month: 'MAR' },
+            { event: 'farmers market', month: 'JUN' },
+        ];
+        console.log({ actual, expected });
+        assert.sameDeepOrderedMembers(actual, expected);
+    });
+
+    it('should fix the sort order for many events', () => {
+        let actual = [
+            { event: 'haunted hayride', month: 'OCT' },
+            { event: 'holiday party', month: 'DEC' },
+            { event: 'picnic', month: 'SEP' },
+            { event: 'dance', month: 'MAR' },
+            { event: 'snowball fight', month: 'FEB' },
+            { event: 'farmers market', month: 'JUN' },
+            { event: 'parade', month: 'JAN' },
+        ];
+        sortByMonth(actual);
+        const expected = [
+            { event: 'parade', month: 'JAN' },
+            { event: 'snowball fight', month: 'FEB' },
+            { event: 'dance', month: 'MAR' },
+            { event: 'farmers market', month: 'JUN' },
+            { event: 'picnic', month: 'SEP' },
+            { event: 'haunted hayride', month: 'OCT' },
+            { event: 'holiday party', month: 'DEC' },
+        ];
+        console.log({ actual, expected });
+        assert.sameDeepOrderedMembers(actual, expected);
+    });
+});
+*/
+
+const MONTHS = [
+    'JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN',
+    'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'
+];
+
+function sortByMonth(events) {
+    
+}
+
+module.exports = sortByMonth;
