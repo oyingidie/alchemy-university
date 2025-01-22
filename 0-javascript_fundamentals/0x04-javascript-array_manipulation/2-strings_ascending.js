2faeffee1e5d47c46ae5c5378b8d437d3490ea7f
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-22 19:40:26 +0100

    Fantastic! Up we go...

---
+++ 0-javascript_fundamentals/0x04-javascript-array_manipulation/2-strings_ascending.js
@@ -0,0 +1,31 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const sortStringsUp = require('../sortStringsUp');
+
+describe('sortStringsUp', () => {
+    it('should mantain sort order for sorted strings', () => {
+        let actual = ['a', 'b', 'c'];
+        sortStringsUp(actual);
+        const expected = ['a', 'b', 'c'];
+        console.log({ actual, expected });
+        assert.sameOrderedMembers(actual, expected);
+    });
+
+    it('should fix sort order for unsorted strings', () => {
+        let actual = ['berries', 'oranges', 'apples', 'limes', 'lemons'];
+        sortStringsUp(actual);
+        const expected = ['apples', 'berries', 'lemons', 'limes', 'oranges'];
+        console.log({ actual, expected });
+        assert.sameOrderedMembers(actual, expected);
+    });
+});
+*/
+
+function sortStringsUp(array) {
+    array.sort((a, b) => {
+        return a.localeCompare(b);
+    });
+}
+
+module.exports = sortStringsUp;
