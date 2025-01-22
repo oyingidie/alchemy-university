c31e2e9fd7567b19d2ffbe349f0b499a44aed2bc
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-22 22:26:11 +0100

    Array map

---
+++ 0-javascript_fundamentals/0x04-javascript-array_manipulation/6-plus_one.js
@@ -0,0 +1,16 @@
+// TEST CASE
+/*
+const plusOne = require('../6-plus_one');
+const {assert} = require('chai');
+
+describe('plus one', () => {
+    it('should add one to each element for a single element', () => {
+        assert.sameOrderedMembers([1], plusOne([0]));
+    });
+
+    it('should add one to each element for several elements', () => {
+        assert.sameOrderedMembers([1, 2, 3], plusOne([0, 1, 2]));
+    });
+});
+*/
+
