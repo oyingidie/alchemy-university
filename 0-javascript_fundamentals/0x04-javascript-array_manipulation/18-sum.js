bd1e9f8cf2ded9a3ed0cce496dd1d2421de9c375
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-23 22:39:58 +0100

    In a summary...

--- 0-javascript_fundamentals/0x04-javascript-array_manipulation/18-sum.js
+++ 0-javascript_fundamentals/0x04-javascript-array_manipulation/18-sum.js
@@ -20,7 +20,7 @@ describe('sum', function () {
 
 // numbers is an array full of numbers
 // let's add all the numbers and return the sum
-// i.e. [1,2,3,4,5] => 15
+// i.e. [1, 2, 3, 4, 5] => 15
 function sum(numbers) {
     return numbers.reduce((accumulator, currentValue) => {
         return accumulator + currentValue; // <-- sum the numbers here!
