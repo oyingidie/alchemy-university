09dc6098000c6e741cc2dc3547e3742e92ad4e74
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-23 11:55:04 +0100

    Array filter

---
+++ 0-javascript_fundamentals/0x04-javascript-array_manipulation/13-only_true.js
@@ -0,0 +1,20 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const onlyTrue = require('../13-only_true');
+
+describe('only true', () => {
+    it('should filter all false values', () => {
+        const expected = [];
+        const actual = onlyTrue([false, false, false]);
+        console.log({expected, actual});
+        assert.sameMembers(expected, actual);
+    });
+    it('should filter mixed false and true values', () => {
+        const expected = [true, true, true];
+        const actual = onlyTrue([false, true, true, false, true]);
+        console.log({expected, actual});
+        assert.sameMembers(expected, actual);
+    });
+});
+*/
