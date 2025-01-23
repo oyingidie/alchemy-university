6dd511b672193546f2a19df6598fb368c8139722
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-23 12:25:19 +0100

    Soft work

---
+++ 0-javascript_fundamentals/0x04-javascript-array_manipulation/14-short_strings.js
@@ -0,0 +1,26 @@
+// TEST CASE
+/*
+const { assert } = require('chai');
+const shortStrings = require('../14-short_strings');
+
+describe('shortStrings', () => {
+    it('should filter a few strings', () => {
+        const expected = ["abc", "a"];
+        const actual = shortStrings(["abc", "abcd", "a"]);
+        console.log({expected, actual});
+        assert.sameMembers(expected, actual);
+    });
+    it('should filter many strings', () => {
+        const expected = ["a", "b", "bat", "cde", "f"];
+        const actual = shortStrings(["a", "apples", "b", "bats", "bat", "pizza", "cde", "f"]);
+        console.log({expected, actual});
+        assert.sameMembers(expected, actual);
+    });
+});
+*/
+
+function shortStrings(array) {
+    return array.filter(element => element.length <= 3);
+}
+
+module.exports = shortStrings;
