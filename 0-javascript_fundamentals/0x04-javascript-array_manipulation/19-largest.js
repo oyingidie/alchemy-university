e40212306509f4a7227754d0c49d507da061e7a3
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-23 22:40:24 +0100

    You are accumulating great knowledge

--- 0-javascript_fundamentals/0x04-javascript-array_manipulation/19-largest.js
+++ 0-javascript_fundamentals/0x04-javascript-array_manipulation/19-largest.js
@@ -20,7 +20,7 @@ describe('largest', function () {
 
 // numbers is an array full of numbers
 // let's find the largest and return it
-// i.e. [2,3,5,1,4] => 5
+// i.e. [2, 3, 5, 1, 4] => 5
 function largest(numbers) {
     return numbers.reduce((accumulator, currentValue) => {
         return accumulator > currentValue
