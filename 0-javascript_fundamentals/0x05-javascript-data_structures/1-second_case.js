894edba7a286a42ab536d03325efeabada5063d9
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-25 00:09:06 +0100

    Recursion

--- 0-javascript_fundamentals/0x05-javascript-data_structures/1-second_case.js
+++ 0-javascript_fundamentals/0x05-javascript-data_structures/1-second_case.js
@@ -1,1 +1,19 @@
+// TEST CASE
+/*
+const factorial = require('../1-second_case');
+const { assert } = require('chai');
 
+describe('factorial', () => {
+    describe('base case', () => {
+        it('should return 1', () => {
+            assert.equal(factorial(1), 1);
+        });
+    });
+
+    describe('more cases', () => {
+        it('should handle 2', () => {
+            assert.equal(factorial(2), 2);
+        });
+    });
+});
+*/
