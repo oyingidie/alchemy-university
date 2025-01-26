44aa7b6a250e2334dd5025db791ea39b95b92a01
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-26 13:51:22 +0100

    Peak village

--- 0-javascript_fundamentals/0x05-javascript-data_structures/7-isempty_and_peek.js
+++ 0-javascript_fundamentals/0x05-javascript-data_structures/7-isempty_and_peek.js
@@ -74,3 +74,33 @@ describe('Stack', function () {
     });
 });
 */
+
+const { MAX_STACK_SIZE } = require('./config');
+
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+    push(item) {
+        if (this.items.length === MAX_STACK_SIZE) {
+            throw new Error("The stack will overflow!");
+        }
+
+        this.items.push(item);
+    }
+    pop() {
+        if (this.items.length === 0) {
+            throw new Error("Underflow-in the stack?!");
+        }
+        
+        return this.items.pop();
+    }
+    isEmpty() {
+        return this.items.length === 0;
+    }
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+}
+
+module.exports = Stack;
