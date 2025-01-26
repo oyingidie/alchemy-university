9d6cc3675ed72f9df9797ec9ea94f254edde0e19
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-26 15:03:07 +0100

    Oh yeah! Pushing for the pop of victory

--- 0-javascript_fundamentals/0x05-javascript-data_structures/9-undo_and_redo.js
+++ 0-javascript_fundamentals/0x05-javascript-data_structures/9-undo_and_redo.js
@@ -41,3 +41,27 @@ describe('Operations Manager', () => {
 });
 */
 
+const Stack = require('./Stack');
+
+class OperationManager {
+    constructor() {
+        this.operations = new Stack();
+        this.undos = new Stack();
+    }
+
+    addOperation(operation) {
+        this.operations.push(operation);
+    }
+
+    undo() {
+        this.undos.push(this.operations.pop());
+    }
+
+    redo() {
+        this.operations.push(this.undos.pop());
+    }
+
+    redoAll() {}
+}
+
+module.exports = OperationManager;
