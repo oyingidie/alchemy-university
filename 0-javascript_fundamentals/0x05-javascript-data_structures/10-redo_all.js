39773e5b162d7e07cead97cb2cd34036c81a416f
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-26 19:09:56 +0100

    Stack your way to the end

--- 0-javascript_fundamentals/0x05-javascript-data_structures/10-redo_all.js
+++ 0-javascript_fundamentals/0x05-javascript-data_structures/10-redo_all.js
@@ -1,5 +1,44 @@
 // TEST CASE
-/**/
+/*
+const OperationsManager = require('../10-redo_all');
+const { assert } = require('chai');
+
+let operationsManager;
+describe('Operations Manager', () => {
+    beforeEach(() => {
+        operationsManager = new OperationsManager();
+    });
+
+    describe('after several undos', () => {
+        const operation1 = "OPERATION_1";
+        const operation2 = "OPERATION_2";
+        const operation3 = "OPERATION_3";
+        beforeEach(() => {
+            operationsManager.addOperation(operation1);
+            operationsManager.addOperation(operation2);
+            operationsManager.addOperation(operation3);
+            operationsManager.undo();
+            operationsManager.undo();
+            operationsManager.undo();
+            operationsManager.redoAll();
+        });
+
+        it('should have the last operation at the top of the operation stack', () => {
+            assert.equal(operationsManager.operations.peek(), operation3);
+        });
+
+        describe('after a subsequent undo', () => {
+            beforeEach(() => {
+                operationsManager.undo();
+            });
+
+            it('should have the second operation at the top of the operation stack', () => {
+                assert.equal(operationsManager.operations.peek(), operation2);
+            });
+        });
+    });
+});
+*/
 
 const Stack = require('./7-isempty_and_peek');
 
