6ef15d44c6ec981d40f41b6894e89738c4f56a79
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-26 14:28:18 +0100

    Push pop and don't you stop

--- 0-javascript_fundamentals/0x05-javascript-data_structures/5-push_and_pop.js
+++ 0-javascript_fundamentals/0x05-javascript-data_structures/5-push_and_pop.js
@@ -38,15 +38,19 @@ class Stack {
     constructor() {
         this.items = [];
     }
+    
     push(item) {
         this.items.push(item);
     }
+    
     pop() {
         return this.items.pop();
     }
+    
     isEmpty() {
         
     }
+    
     peek() {
         
     }
