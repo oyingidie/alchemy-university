3057a1511e64383e7187b8387883a1a7f1fd093a
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-27 15:42:20 +0100

    Linked list

--- 0-javascript_fundamentals/0x05-javascript-data_structures/12-linked_list.js
+++ 0-javascript_fundamentals/0x05-javascript-data_structures/12-linked_list.js
@@ -1,1 +1,15 @@
+// TEST CASE
+/*
+const LinkedList = require('../12-linked_list');
+const Node = require('../11-node');
+const { assert } = require('chai');
+
+describe('LinkedList instance', () => {
+    const list = new LinkedList();
+
+    it('should start with a null head', () => {
+        assert.strictEqual(list.head, null);
+    });
+});
+*/
 
