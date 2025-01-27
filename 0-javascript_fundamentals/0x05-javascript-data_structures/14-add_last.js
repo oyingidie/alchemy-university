8622e3a39495bd2cb26820f10be91c47cfe9c9fa
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-27 16:38:04 +0100

    These nodes traveled

---
+++ 0-javascript_fundamentals/0x05-javascript-data_structures/14-add_last.js
@@ -0,0 +1,87 @@
+// TEST CASE
+/*
+const LinkedList = require('../14-add_last');
+const Node = require('../11-node');
+const { assert } = require('chai');
+
+describe('LinkedList instance', () => {
+    const list = new LinkedList();
+
+    it('should start with a null head', () => {
+        assert.strictEqual(list.head, null);
+    });
+
+    describe('after adding a new node last', () => {
+        const node1 = new Node(1);
+        before(() => {
+            list.addLast(node1);
+        });
+
+        it('should set the head to the new node', () => {
+            assert.equal(list.head, node1);
+        });
+
+        describe('after adding another node first', () => {
+            const node2 = new Node(2);
+            before(() => {
+                list.addFirst(node2);
+            });
+
+            it('should set the head to the new node', () => {
+                assert.equal(list.head, node2);
+            });
+
+            it('should set the next on the new head', () => {
+                assert.equal(node2.next, node1);
+            });
+
+            describe('after adding another node last', () => {
+                const node3 = new Node(3);
+                before(() => {
+                    list.addLast(node3);
+                });
+
+                it('should keep the head as the previous node', () => {
+                    assert.equal(list.head, node2);
+                });
+
+                it('should set this node after the middle node', () => {
+                    assert.equal(node1.next, node3);
+                });
+
+                it('should set this node to the end', () => {
+                    assert.equal(node3.next, null);
+                });
+            });
+        });
+    });
+});
+*/
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    addFirst(node) {
+        node.next = this.head;
+        this.head = node;
+    }
+
+    addLast(node) {
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        
+        let tail = this.head;
+
+        while (tail.next) {
+            tail = tail.next;
+        }
+
+        tail.next = node;
+    }
+}
+
+module.exports = LinkedList;
