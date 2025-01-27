d418d30d90393677dbb363d708baf47838b9a7f8
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-27 15:33:16 +0100

    Linked list

---
+++ 0-javascript_fundamentals/0x05-javascript-data_structures/11-node.js
@@ -0,0 +1,19 @@
+// TEST CASE
+/*
+const Node = require('../11-node');
+const { assert } = require('chai');
+
+describe('node', () => {
+    const data = 3;
+    const node = new Node(data);
+
+    it('should store data', () => {
+        assert.equal(node.data, 3);
+    });
+
+    it('should have a null next', () => {
+        assert.strictEqual(node.next, null);
+    });
+});
+*/
+
