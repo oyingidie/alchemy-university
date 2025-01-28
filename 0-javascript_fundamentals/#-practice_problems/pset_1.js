e8436859a4282fd274dc32873e7772b09935bdb3
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-28 09:48:19 +0100

    Royal flush, maybe?

--- 0-javascript_fundamentals/#-practice_problems/pset_1.js
+++ 0-javascript_fundamentals/#-practice_problems/pset_1.js
@@ -128,7 +128,7 @@ function playerHandScore(hand) {
         "K": 4,
         "Q": 3,
         "J": 2
-    }
+    };
 
     for (let i = 0; i < hand.length; i++) {
         score += cardPoints[hand[i]];
