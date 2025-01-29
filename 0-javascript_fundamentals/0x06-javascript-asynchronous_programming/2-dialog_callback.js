f8741c5846fa132e093043d386f1a23d85630c6b
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-29 16:34:03 +0100

    Callback functions

---
+++ 0-javascript_fundamentals/0x06-javascript-asynchronous_programming/2-dialog_callback.js
@@ -0,0 +1,18 @@
+// TEST CASE
+/*
+const Dialog = require('../2-dialog_callback');
+const { assert } = require('chai');
+
+describe('Dialog', () => {
+    it('should allow me to register a callback', () => {
+        const dialog = new Dialog();
+        let called = false;
+        dialog.onClose(() => {
+            called = true;
+        });
+        dialog.close();
+        assert(called, "Callback was not called after close");
+    });
+});
+*/
+
