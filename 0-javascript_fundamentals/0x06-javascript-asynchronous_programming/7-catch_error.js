abd8a70b39d07b96a57ff56e8dc9da9a86dbfc86
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-29 21:00:10 +0100

    Nice catch

--- 0-javascript_fundamentals/0x06-javascript-asynchronous_programming/7-catch_error.js
+++ 0-javascript_fundamentals/0x06-javascript-asynchronous_programming/7-catch_error.js
@@ -63,3 +63,21 @@ describe('successful order', () => {
     });
 });
 */
+
+const { makeFood } = require('./Kitchen');
+
+class Order {
+    constructor() {
+        this.isReady = false;
+    }
+
+    request(food) {
+        makeFood(food).then(() => {
+            this.isReady = true;
+        }).catch((error) => {
+            this.error = error;
+        });
+    }
+}
+
+module.exports = Order;
