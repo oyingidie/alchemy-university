6b3acb172e657a39bff0d4211570e1ac2d53914a
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-30 19:08:11 +0100

    You used to callback on my cellphone

--- 0-javascript_fundamentals/0x06-javascript-asynchronous_programming/16-multiple_callbacks.js
+++ 0-javascript_fundamentals/0x06-javascript-asynchronous_programming/16-multiple_callbacks.js
@@ -1,4 +1,52 @@
 // TEST CASE
+/*
+const Pact = require('../16-multiple_callbacks');
+const { assert } = require('chai');
+
+describe('Pact', function () {
+    it('should run both .then callbacks', (done) => {
+        let val1;
+        let val2;
+        let pact = new Pact((resolve, reject) => {
+            setTimeout(() => {
+                resolve(42);
+            }, 100);
+        });
+        pact.then((val) => {
+            val1 = val;
+        });
+        pact.then((val) => {
+            val2 = val;
+        });
+        setTimeout(() => {
+            assert.equal(val1, 42, "First then callback did not succeed");
+            assert.equal(val2, 42, "Second then callback did not succeed");
+            done();
+        }, 150)
+    });
+
+    it('should run both .catch callbacks', (done) => {
+        let val1;
+        let val2;
+        let pact = new Pact((resolve, reject) => {
+            setTimeout(() => {
+                reject(42);
+            }, 100);
+        });
+        pact.catch((val) => {
+            val1 = val;
+        });
+        pact.catch((val) => {
+            val2 = val;
+        });
+        setTimeout(() => {
+            assert.equal(val1, 42, "First catch callback did not succeed");
+            assert.equal(val2, 42, "Second catch callback did not succeed");
+            done();
+        }, 150)
+    });
+});
+*/
 
 class Pact {
     // add methods to return on the instance
