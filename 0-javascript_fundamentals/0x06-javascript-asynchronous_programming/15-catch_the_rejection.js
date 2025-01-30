90caa5ec341d752d3b5aca6ad8430bda47c760df
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-30 17:35:25 +0100

    Sorry, I'm busy Saturday

--- 0-javascript_fundamentals/0x06-javascript-asynchronous_programming/15-catch_the_rejection.js
+++ 0-javascript_fundamentals/0x06-javascript-asynchronous_programming/15-catch_the_rejection.js
@@ -1,1 +1,40 @@
+// TEST CASE
+/*
+const Pact = require('../15-catch_the_rejection');
+const { assert } = require('chai');
 
+describe('Pact', function () {
+    it('should call a .catch function with the reject value after a timeout', (done) => {
+        let value;
+        new Pact((resolve, reject) => {
+            setTimeout(() => {
+                reject(42);
+            }, 100);
+        }).catch((err) => {
+            assert.equal(err, 42);
+            done();
+        });
+    });
+});
+*/
+
+class Pact {
+    // add methods to return on the instance
+    constructor(executor) {
+        executor((value) => {
+            this.thenCallback(value);
+        }, (value) => {
+            this.catch(value);
+        });
+    }
+
+    then(thenCallback) {
+        this.thenCallback = thenCallback;
+    }
+
+    catch(exception) {
+        this.catch = exception;
+    }
+}
+
+module.exports = Pact;
