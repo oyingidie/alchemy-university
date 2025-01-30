05616433889a6d5241ff2a549793faa5b53f4a64
oyingidie <114517560+oyingidie@users.noreply.github.com>
2025-01-30 11:24:08 +0100

    The shape of patience

--- 0-javascript_fundamentals/0x06-javascript-asynchronous_programming/10-async_await.js
+++ 0-javascript_fundamentals/0x06-javascript-asynchronous_programming/10-async_await.js
@@ -50,3 +50,15 @@ describe('Lab Results', () => {
     });
 });
 */
+
+const { getResults } = require('./lab');
+const { sendResults } = require('./messaging');
+const { logResponse, logError } = require('./logs');
+
+async function handleResults(patientId) {
+    const results = await getResults(patientId);
+    const response = await sendResults(patientId, results);
+    await logResponse(response);
+}
+
+module.exports = handleResults;
